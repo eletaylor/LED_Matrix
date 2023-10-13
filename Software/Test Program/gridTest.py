d4e3718743da4b726507aac3b1a376864229e82a
akridgej <123841739+akridgej@users.noreply.github.com>
2023-10-13 11:16:48 -0600

    All bugs have been fixed! (for now :'/)

--- Software/Test Program/gridTest.py
+++ Software/Test Program/gridTest.py
@@ -5,29 +5,31 @@ from tkinter import messagebox
 def createGrid():
     for i in range(rows):
         for j in range(columns):
-            btns[i][j] = Button(tk, padx=10, bg="gray")
+            btns[i][j] = Button(tk, bg="gray")
             btns[i][j]['command'] = lambda btn=btns[i][j]: color(btn)
             btns[i][j].grid(row=i, column=j)
 
 
 def createColorButtons():
     for i in range(len(colors)):
-        if (colors[i] == "White" or colors[i] == "Yellow"):
+        if colors[i] == "White" or colors[i] == "Yellow":
             colorButtons[i] = Button(tk, text=colors[i], fg='black', bg=colors[i].lower())
         else:
             colorButtons[i] = Button(tk, text=colors[i], fg='white', bg=colors[i].lower())
 
         colorButtons[i]['command'] = lambda btn=colorButtons[i]: changeColor(btn)
-        colorButtons[i].grid(row=i+5, column=20)
+        colorButtons[i].grid(row=i + 5, column=20)
 
 
 def createClearandErase():
+    global clearButton
     clearButton = Button(tk, text="Clear", fg="white", bg="gray")
-    clearButton.grid(row=16, column=20, pady = 8)
+    clearButton.grid(row=13, column=20)
     clearButton['command'] = lambda btn=clearButton: clear(btn)
 
+    global eraseButton
     eraseButton = Button(tk, text="Erase", fg="white", bg="gray")
-    eraseButton.grid(row=18, column=20, pady = 8)
+    eraseButton.grid(row=14, column=20)
     eraseButton['command'] = lambda btn=eraseButton: eraser(btn)
 
 
@@ -37,6 +39,7 @@ def clear(btn):
         for j in range(columns):
             btns[i][j]['bg'] = "gray"
 
+
 def resetColor():
     global userColor
     userColor = blankColor
@@ -45,14 +48,12 @@ def resetColor():
 def eraser(btn):
     global erase
     erase = True
-    print("Erased")
 
 
 def color(btn):
-   
-    if (userColor == blankColor):
+    if userColor == blankColor and erase is False:
         print("No color selected")
-    elif (erase == True):
+    elif erase:
         btn['bg'] = blankColor
     else:
         btn['bg'] = userColor
@@ -85,13 +86,7 @@ erase = False
 
 tk = Tk()
 tk.title("Pixel Board")
-
-createGrid()
-createColorButtons()
-createClearandErase()
-
-tk.protocol("WM_DELETE_WINDOW", on_closing)
-tk.mainloop()
+tk.attributes('-fullscreen', True)
 
 createGrid()
 createColorButtons()
