afcab39c959f5d7fbd38c0b7c02ea47f4fa7c7a6
Alex Rodriguez <99780537+roddy-54@users.noreply.github.com>
2023-11-01 15:54:36 -0600

    Update exe.py
    The most up to date version of the GUI application as of November 1, 2023. On the Pi as well.

--- Software/Test Program/exe.py
+++ Software/Test Program/exe.py
@@ -1,20 +1,188 @@
 from tkinter import *
+from tkinter import messagebox
+from tkinter import filedialog
+import board
+import neopixel
+import os
+
+#data_pin is for the data pin to be wired to. 
+#Must use GPIO10 for sudoless control of leds (physical pin 19)
+data_pin = board.D10
+
+#num_lights holds the number of LED's to be programmed
+num_lights = 64
+
+pixel1 = neopixel.NeoPixel(
+    data_pin, num_lights, brightness = 0.2,
+    auto_write = True, pixel_order = neopixel.RGB
+    )
+
+
+
+def createGrid():
+    for i in range(rows):
+        for j in range(columns):
+            btns[i][j] = Button(tk, bg="gray")
+            btns[i][j]['command'] = lambda btn=btns[i][j]: color(btn)
+            btns[i][j].grid(row=i+1, column=j)
+            Grid.rowconfigure(tk, index = i+1, weight = 1)
+
+
+def createColorButtons():
+    for i in range(len(colors)):
+        if colors[i] == "White" or colors[i] == "Yellow":
+            colorButtons[i] = Button(tk, text=colors[i], fg='black', bg=colors[i].lower())
+        else:
+            colorButtons[i] = Button(tk, text=colors[i], fg='white', bg=colors[i].lower())
+
+        colorButtons[i]['command'] = lambda btn=colorButtons[i]: changeColor(btn)
+        colorButtons[i].grid(row=i+7, column=35)
+
+
+def createClearAndErase():
+    global clearButton
+    clearButton = Button(tk, text="Clear", fg="white", bg="gray")
+    clearButton.grid(row=15, column=35)
+    clearButton['command'] = lambda btn=clearButton: clear(btn)
+
+    global eraseButton
+    eraseButton = Button(tk, text="Erase", fg="white", bg="gray")
+    eraseButton.grid(row=16, column=35)
+    eraseButton['command'] = lambda btn=eraseButton: eraser(btn)
+
+
+def createMenuTab():
+    global menuStrip
+    global file
+
+    menuStrip = Menu(tk)
+    file = Menu(menuStrip, tearoff=0)
+
+    menuStrip.add_cascade(label="File", menu=file)
+    file.add_command(label="Open Design", command=lambda: open_file())
+    file.add_command(label="Save Design", command=lambda: save_file())
+    menuStrip.add_command(label="Exit", command = lambda: on_closing())
+
+
+def createFolders():
+    if not(os.path.exists(imagePath)):
+        os.makedirs(imagePath)
+    if not(os.path.exists(textPath)):
+        os.makedirs(textPath)
+
+def clear(btn):
+    global boardClear
+    if boardClear is False:
+        if messagebox.askyesno("Confirm Clear", "Are you sure you want to clear?"):
+            reset()
+            for i in range(rows):
+                for j in range(columns):
+                    btns[i][j]['bg'] = "gray"
+    else:
+        messagebox.showinfo("Grid Empty", "Grid empty. Nothing to clear.")
+
+
+def reset():
+    global userColor
+    global erase
+    global boardClear
+    userColor = blankColor
+    erase = False
+    boardClear = True
+
+
+def eraser(btn):
+    global erase
+    global boardClear
+    if boardClear is False:
+        messagebox.showinfo("Erase Activated", "Erase activated.")
+        erase = True
+    else:
+        messagebox.showinfo("Grid Empty", "Grid empty. Nothing to erase.")
+
+
+def color(btn):
+    global boardClear
+    if userColor == blankColor and erase is False:
+        messagebox.showinfo("No Color Selected", "No color selected. Please choose a color on the right-hand side "
+                            + "of the screen.")
+    elif erase:
+        btn['bg'] = blankColor
+        boardClear = False
+    else:
+        btn['bg'] = userColor
+        boardClear = False
+
+
+def changeColor(btn):
+    global userColor
+    userColor = btn['bg']
+
+    global erase
+    if erase:
+        erase = False
+        messagebox.showinfo("Color Changed", "Erase deactivated. The active color is " + userColor)
+    else:
+        messagebox.showinfo("Color Changed", "The active color is " + userColor)
+
+
+def on_closing():
+    if messagebox.askokcancel("Quit", "Are you sure you want to quit?"):
+        reset()
+        tk.destroy()
+
+def open_file():
+    openFile = filedialog.askopenfile(initialdir = imagePath)
+
+def save_file():
+    saveFile = filedialog.asksaveasfile(defaultextension=".png", initialdir = imagePath)
+        
+        
+        
+def getIndex(i, j):
+    if j <= 7:
+        if i % 2 == 0:
+            index = 8*i + j
+        else:
+            index = 8*i + (8-(j+1))
+    else:
+        if i % 2 == 0:
+            index = 8*(15+i) + j
+        else:
+            index = 8*(16+i) + (16-(j+1))
+
 
 rows = 16
 columns = 16
-
 btns = [[None for i in range(rows)] for j in range(columns)]
-master = Tk()
-master.title("My First Application!")
-master.geometry("500x500")
+colors = ["White", "Red", "Green", "Blue", "Black", "Cyan", "Yellow"]
+colorButtons = [None for i in range(len(colors))]
+clearButton = None
+eraseButton = None
+menuStrip = None
+file = None
+blankColor = "gray"
+userColor = blankColor
+erase = False
+boardClear = True
+imagePath = os.getcwd() + "/Pixel Box Images/Images"
+textPath = os.getcwd() + "/Pixel Box Images/Colors"
 
-def changeColor(btn):
-    btn.configure(bg="blue")
+tk = Tk()
+tk.title("Pixel Board")
 
-for i in range(rows):
-    for j in range(columns):
-        btns[i][j]=Button(master, padx=10, bg="white")
-        btns[i][j].grid(row = i, column = j)
-        btns[i][j]['command'] = lambda btn=btns[i][j]:changeColor(btn)
+createGrid()
+createColorButtons()
+createClearAndErase()
+createMenuTab()
+createFolders()
 
-master.mainloop()
+tk.config(menu=menuStrip)
+tk.attributes('-fullscreen', True)
+           
+tk.protocol("WM_DELETE_WINDOW", on_closing)
+tk.mainloop()
+
+
+
+
